958e01198bbac35f72ee53ff5f24b847a15b26fb
ItsMeeeeeeee <yl1381@georgetown.edu>
2022-04-18 13:23:24 -0400

    update

---
+++ database/__init__.py
@@ -0,0 +1,44 @@
+
+
+from email.policy import default
+
+
+class Table:
+    def __init__(self, name, var_type):
+        self.name = name
+        self.var = []
+        self.type = []
+        self.data = {}
+        self._init_var_type(var_type)
+        for col in self.var:
+            self.data[col] = []
+        self.primary = self._checkPrimary()
+        self.index = 0
+        self.data[self.primary] = []
+
+    def _init_var_type(self, var_type):
+        for var, type in self.var_type.items():
+            self.var.append(var)
+            self.type.append(type)
+
+    def _checkPrimary(self):
+        for key, type in self.var_type.items():
+            if 'primary key' in type:
+                return key
+        return '__index__'
+    
+    def insert(self, action):
+        if not action.get('data', default=None) == None:
+            for col in self.var:
+                self.data[col].append(action.get(col, default=None))
+        # else:
+
+        # check if the table got user defiend primary key, and append it
+        if self.primary == '__index__':
+            index = self.index 
+            self.index += 1
+            self.data[self.primary].append(index)
+
+        # append other values 
+        
+
--- executer/__init__.py
+++ executer/__init__.py
@@ -1,12 +1,22 @@
 from parser import SQLParser
+from database import Table
 
 class SQLExecuter:
-    def __init__(self):
+    def __init__(self, tables):
         self.parser = SQLExecuter()
+        self.tables = {}
+        self._init_tables(tables)
+        self.function = {
+            'insert' : self._insert
+        }
     
-    def insert(self, statement):
+    def _init_tables(self, tables):
+        for name, table in tables.items():
+            self.tables[name] = table
+
+    def execute(self, statement):
         action = self.parser.parse(statement)
-        if action['data'] == None:
-            print()
-        else:
-            print()
+        self.function[action['type']](action)
+
+    def _insert(self, action):
+        self.tables[action['table']].insert(action)
